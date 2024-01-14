a2301fadbeff490900333d47d86355278cfe82b9
reasonablep <140934359+reasonablep@users.noreply.github.com>
2024-01-13 22:36:42 -0800

    Completed webpack config file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,11 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        chunks: ['main']
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install']
+      }),
+      new WebpackPwaManifest ({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple and lightweight PWA-based text editor',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+          },
+        ],
+      }),
+
+      new InjectManifest ({
+        swSrc: './src/service-worker.js',
+        swDest: 'service-worker.js'
+      }),
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel.preset-env']
+            },
+          },
+        },
         
       ],
     },
